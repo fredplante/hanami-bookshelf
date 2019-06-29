fed683f2d06fcc66e0e528a1381b244dc26b645b
Frédéric Planté <f.plante@stuart.com>
2019-06-29 14:24:17 +0200

    finish bookshelf hanami tutorial

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -1,5 +1,5 @@
-# Configure your routes here
-# See: http://hanamirb.org/guides/routing/overview/
-#
-# Example:
-# get '/hello', to: ->(env) { [200, {}, ['Hello from Hanami!']] }
+root to: 'home#index'
+get '/books', to: 'books#index'
+get '/books/new', to: 'books#new'
+post '/books', to: 'books#create'
+post '/books', to: 'books#create'
